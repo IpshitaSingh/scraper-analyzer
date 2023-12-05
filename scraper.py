19272688cb9f8971c55b6ee2f52f4b381e5d192b
ipshitasingh <ipshita0501@gmail.com>
2023-12-05 22:25:33 +0530

    updated scraper code

--- scraper.py
+++ scraper.py
@@ -5,12 +5,12 @@ import pandas as pd
 url = 'https://www.scrapethissite.com/pages/simple/'
 
 def get_soup(url):
-    # Send a GET request to the specified URL
+    #send request to the specified URL
     response = requests.get(url)
     
-    # Check if the request was successful 
+    #checking if the request was successful 
     if response.status_code == 200:
-        # Parse the HTML content of the page
+        #extract the HTML content of the page
         soup = BeautifulSoup(response.content, 'html.parser')
         return soup
     else:
@@ -19,7 +19,7 @@ def get_soup(url):
 
 def extract_country_data(country_element):
     try:
-        # Extract data from the specific elements within the country element
+        #extract relevant data & return as dictionary (country name, capital, population, area)
         country_name = country_element.find('h3', class_ ="country-name").text.strip()
         capital = country_element.find('span', class_ = "country-capital").text.strip()
         population = int(country_element.find('span', class_ ="country-population").text.strip().replace(',', ''))
@@ -35,24 +35,25 @@ def extract_country_data(country_element):
         print(f"Error while extracting data: {e}")
         return None
 
-def scrape_country_data(url):
+def create_df(url):
+    #scrape data from the website and create a dataframe
     soup = get_soup(url)
     
     if soup:
-        # Find the HTML elements containing country data
+        #find the HTML elements containing country data
         country_elements = soup.find_all('div', class_='country')
         
-        # Extract data from each country element
+        #extract data from each country element
         data_list = [extract_country_data(country_element) for country_element in country_elements if country_element]
 
-        # Create a Pandas DataFrame from the list of dictionaries
+        #create the df from the list of dictionaries
         df = pd.DataFrame(data_list)
         return df
     else:
         return None
 
-data_frame = scrape_country_data(url)
+data_frame = create_df(url)
 
-# If the DataFrame is not empty, you can now use it for visualization or analysis.
+#print the DF if it's not empty
 if data_frame is not None:
     print(data_frame)
